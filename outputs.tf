7aab27d2a40828b68b051fc2ae4cd7780701427f
Daniel Allen <dan@aeternagroup.net>
2026-08-13 12:44:00 +0100

    Update to schema version v6.58.0

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+output "prometheus_scraper_logging_configurations_id" {
+  description = "Map of id values across all prometheus_scraper_logging_configurations, keyed the same as var.prometheus_scraper_logging_configurations"
+  value       = { for k, v in aws_prometheus_scraper_logging_configuration.prometheus_scraper_logging_configurations : k => v.id if v.id != null && length(v.id) > 0 }
+}
+output "prometheus_scraper_logging_configurations_logging_destination" {
+  description = "Map of logging_destination values across all prometheus_scraper_logging_configurations, keyed the same as var.prometheus_scraper_logging_configurations"
+  value       = { for k, v in aws_prometheus_scraper_logging_configuration.prometheus_scraper_logging_configurations : k => v.logging_destination if v.logging_destination != null && length(v.logging_destination) > 0 }
+}
+output "prometheus_scraper_logging_configurations_region" {
+  description = "Map of region values across all prometheus_scraper_logging_configurations, keyed the same as var.prometheus_scraper_logging_configurations"
+  value       = { for k, v in aws_prometheus_scraper_logging_configuration.prometheus_scraper_logging_configurations : k => v.region if v.region != null && length(v.region) > 0 }
+}
+output "prometheus_scraper_logging_configurations_scraper_components" {
+  description = "Map of scraper_components values across all prometheus_scraper_logging_configurations, keyed the same as var.prometheus_scraper_logging_configurations"
+  value       = { for k, v in aws_prometheus_scraper_logging_configuration.prometheus_scraper_logging_configurations : k => v.scraper_components if v.scraper_components != null && length(v.scraper_components) > 0 }
+}
+output "prometheus_scraper_logging_configurations_scraper_id" {
+  description = "Map of scraper_id values across all prometheus_scraper_logging_configurations, keyed the same as var.prometheus_scraper_logging_configurations"
+  value       = { for k, v in aws_prometheus_scraper_logging_configuration.prometheus_scraper_logging_configurations : k => v.scraper_id if v.scraper_id != null && length(v.scraper_id) > 0 }
+}
+
